678a879a8ba5520427a51889900dfaed8dc9afb7
AcmenLu <1109325089@qq.com>
2018-05-27 21:44:47 +0800

    完善String类
    完善String类

--- AcmenEngine/Include/Common/String.h
+++ AcmenEngine/Include/Common/String.h
@@ -7,38 +7,43 @@ class String
 {
 public:
 	static _bool StartWith( const String& srcstr, const String& desstr );
+	static _dword StrToDword( const String& srcstr );
+	static _float StrToFloat( const String& srcstr );
 
 private:
 	_char*			mString;
-	_long			mSize;
 	_long			mLength;
+	_long			mSize;
 
 public:
 	inline String( )
-		:mString( _null ), mSize( 0 ){ }
-	String( const _char* str );
+		:mString( _null ), mLength( 0 ), mSize( 0 ){ }
 	inline String( String* str )
-		: mString( (_char*)str ), mSize( str->Size( ) ), mLength( str->Length( ) ){ }
+		: mString( (_char*)str ), mLength( str->Length( ) ), mSize( str->Size( ) ){ }
 
-	inline ~String( )
-		{ if ( mString != _null ) delete mString; mSize = 0; }
+	String( const _char* str );
+	~String( );
 
 	inline operator const _char* ( ) const
 		{ return mString; }
 	inline _char operator [] ( _long index ) const
 		{ return mString[ index ]; }
+	inline _char* GetData( )
+		{ return mString; }
 
-	inline _long Size( )
+	inline _long Size( ) const
 		{ return mSize; }
-	inline _long Length( )
+	inline _long Length( ) const
 		{ return mLength; }
 
 	_void	Grow( _dword length );
 	String&	operator = ( const _char* chrs );
 	String&	operator += ( _char ch );
 	String&	operator += ( const _char* str );
-
-private:
-
+	String	SubString( _long start, _long end = -1 );
+	String& TrimStart( const _char ch = ' ' );
+	String& TrimEnd( const _char ch = ' ' );
+	String& Trim( const _char ch = ' ' );
+	vector<String*>& Split( const _char str, vector<String*> &list );
 };
 }
--- AcmenEngine/Source/Common/String.cpp
+++ AcmenEngine/Source/Common/String.cpp
@@ -2,12 +2,13 @@
 
 _bool String::StartWith( const String& srcstr, const String& desstr )
 {
-	_dword slen = ::strlen( srcstr );
-	_dword dlen = ::strlen( desstr );
-	if ( slen < dlen )
+	if ( srcstr.Length( ) <= 0 || desstr.Length( ) <= 0 )
 		return _false;
 
-	for( _dword i = 0; i < dlen; i ++ )
+	if ( srcstr.Length( ) < desstr.Length( ) )
+		return _false;
+
+	for( _dword i = 0; i < desstr.Length( ); i ++ )
 	{
 		if ( desstr[i] != srcstr[i] )
 			return _false;
@@ -15,11 +16,40 @@ _bool String::StartWith( const String& srcstr, const String& desstr )
 	return _true;
 }
 
-String::String( const _char* str ): mLength( 0 ), mSize( 0 )
+_dword String::StrToDword( const String& srcstr )
+{
+	_char* tmp = new _char[srcstr.Length( )];
+	for ( _dword i = 0; i < srcstr.Length( ); i ++ )
+		tmp[i] = srcstr[i];
+	return ::atoi( tmp );
+}
+
+_float String::StrToFloat( const String& srcstr )
+{
+	_char* tmp = new _char[srcstr.Length( )];
+	for ( _dword i = 0; i < srcstr.Length( ); i ++ )
+		tmp[i] = srcstr[i];
+	return ::atof( tmp );
+}
+
+String::String( const _char* str )
+	: mString( _null ), mLength( 0 ), mSize( 0 )
 {
 	_dword len = ::strlen( str );
+	if ( len <= 0 )
+		return;
+
 	Grow( len );
 	::memcpy( mString, str, len );
+	mString[len] = '\0';
+	mLength = len;
+}
+
+String::~String( )
+{
+	mLength = 0;
+	mSize = 0;
+	SAFE_RELESE( mString );
 }
 
 _void String::Grow( _dword length )
@@ -28,39 +58,35 @@ _void String::Grow( _dword length )
 		return;
 
 	if ( mSize == 0 )
-	{
 		mSize = 32;
-		while ( length > mSize )
-			mSize *= 2;
 
-		mString = new _char[mSize];
-		::memset( mString, 0, mSize );
-	}
-	else
+	while ( mLength + length + 1 > mSize )
+		mSize *= 2;
+
+	_char* tmp = new _char[mSize];
+	::memset( tmp, 0, mSize );
+	if ( mString != _null )
 	{
-		_long len = mSize * 2;
-		while( mLength + length > len )
-			len *= 2;
-
-		_char* tmp = new _char[len];
-		::memset( tmp, 0, len );
 		::memcpy( mString, tmp, mLength );
-		delete mString;
-		mString = tmp;
-		mSize = len;
+		SAFE_RELESE( mString );
 	}
+	mString = tmp;
 }
 
 String&	String::operator = ( const _char* chrs )
 {
-	if ( mString != _null )
-		delete mString;
+	SAFE_RELESE( mString );
+	mLength = 0;
+	mSize = 0;
 
 	_dword len = ::strlen( chrs );
-	mString = new _char[len + 1];
-	::memcpy( mString, chrs, len );
-	mString[len] = '\0';
-	mLength = mSize = len + 1;
+	if ( len > 0 )
+	{
+		Grow( len );
+		::memcpy( mString, chrs, len );
+		mString[len] = '\0';
+		mLength = len;
+	}
 	return *this;
 }
 
@@ -68,6 +94,7 @@ String&	String::operator += ( _char ch )
 {
 	Grow( 1 );
 	mString[mLength++] = ch;
+	mString[mLength] = '\0';
 	return *this;
 }
 
@@ -82,5 +109,92 @@ String&	String::operator += ( const _char* str )
 
 		mString[mLength++] = str[i];
 	}
+	mString[mLength] = '\0';
 	return *this;
+}
+
+String String::SubString( _long start, _long end )
+{
+	if ( start < 0 )
+		return String( );
+
+	if ( end == -1 || end > mLength )
+		end = mLength;
+
+	if ( end < start )
+		return String( );
+
+	_dword len = end - start;
+	_char* cha = new _char[len];
+	for ( _dword i = start; i < end; i ++ )
+		cha[i] = mString[i - start];
+	return String( cha );
+}
+
+String& String::TrimStart( const _char ch )
+{
+	_long index = -1;
+	for ( _dword i = 0; i < mLength; i ++ )
+	{
+		if ( mString[i] != ch )
+			break;
+
+		index = i;
+	}
+	if ( index >= 0 )
+	{
+		for ( _dword i = index; i < mLength; i ++ )
+			mString[i - index] = mString[i];
+		
+		mLength -= ( index + 1 );
+		mString[mLength] = '\0';
+	}
+	return *this;
+}
+
+String& String::TrimEnd( const _char ch )
+{
+	if ( mString != _null )
+	{
+		for ( _dword i = mLength - 1; i >= 0; i -- )
+		{
+			if ( mString[i] != ch )
+				break;
+			mString[--mLength] = '\0';
+		}
+	}
+	return *this;
+}
+
+String& String::Trim( const _char ch )
+{
+	TrimStart( ch );
+	TrimEnd( ch );
+	return *this;
+}
+
+vector<String*> & String::Split( const _char str, vector<String*> &list )
+{
+	if ( str == _null )
+		return list;
+
+	_dword head = 0, tail = 0;
+	while( mString[tail] != '\0')
+	{
+		if ( mString[tail] == str )
+		{
+			String* s = new String( );
+			while( head < tail )
+			{
+				(*s) += mString[head];
+				head ++;
+			}
+			list.push_back( s );
+			head = tail + 1;
+		}
+		tail ++;
+	}
+
+	list.push_back( new String( mString + head ) );
+	return list;
 }
