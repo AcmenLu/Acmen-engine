8d994578c1a0bb6d967c0f250ec05059710950aa
AcmenLu <1109325089@qq.com>
2018-05-27 21:46:18 +0800

    修改mesh类的结构
    修改mesh类的结构

--- AcmenEngine/Include/Common/Mesh.h
+++ AcmenEngine/Include/Common/Mesh.h
@@ -13,19 +13,20 @@ class Mesh : public RenderObject
 private:
 	vector< Vertex >		mVertices;
 	vector< _dword >		mIndices;
-	vector< Mesh* >			mMeshs;
 	Material*				mMaterial;
 	Shader*					mShader;
 	_dword					mVAO;
-	string					mResName;
+	String					mResName;
+	_bool					mIsInited;
 
 public:
 	Matrix4					mTransform;
 
 public:
 	inline Mesh( )
-		: mTransform( Matrix4( ) ),mShader( _null ){ }
-	Mesh( const string& filename );
+		: mTransform( Matrix4( ) ),mShader( _null ), mVAO( 0 ), mResName( "" ), mIsInited( _false ){ }
+	Mesh( const String& filename )
+		: mMaterial( _null ), mShader( _null ), mVAO( 0 ), mResName( "" ), mIsInited( _false ){ }
 	Mesh( vector< Vertex > vertices, vector< _dword > indices, Material* material );
 	~Mesh( );
 
@@ -34,16 +35,21 @@ public:
 	inline Matrix4 GetTransform( )
 		{ return mTransform; }
 
-	_void InitData( const string& filename );
+	inline _void SetVertexs( vector< Vertex > vertices )
+		{ mVertices = vertices; }
+	inline vector<Vertex>& GetVertexs( )
+		{ return mVertices; }
+
+	inline _void SetIndices( vector< _dword > indices )
+		{ mIndices = indices; }
+	inline vector< _dword >& GetIndices( )
+		{ return mIndices; }
+
+	_void InitMesh( );
 	_void InitVAO( );
 	_void InitShader( );
 	_void BindShaderData( );
 	_void Render( );
-
-private:
-	_void ProcessNode( aiNode *node, const aiScene *scene );
-	Mesh* ProcessMesh( aiMesh *mesh, const aiScene *scene );
-	Texture* LoadMaterialTexture( aiMaterial *mat, aiTextureType type );
 };
 
 };
